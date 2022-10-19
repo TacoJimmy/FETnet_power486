acf268b91d27ad40946a05c332c96676e8365e4b
TacoJimmy <t5669016@ntut.org.tw>
2022-10-19 14:13:36 +0800

    Update FET_Flask.py

--- FET_Flask.py
+++ FET_Flask.py
@@ -1,7 +1,31 @@
 from flask import Flask, render_template, request, jsonify, json
+import schedule  
+import time  
+from flask import Flask
+from flask_apscheduler import APScheduler
+import FET_MQTT
+import FET_modbusrtu
 
 app = Flask(__name__)
 
+class Config(object):
+    JOBS = [
+        {
+            'id': 'publish_PowerMeter',  
+            'func': '__main__:publish_PowerMeter',
+            'args': (1, 2),   
+            'trigger': 'interval',
+            'seconds': 60 
+        },
+        {
+            'id': 'read_com1',  
+            'func': '__main__:read_com1',
+            'args': (1, 2),   
+            'trigger': 'interval',
+            'seconds': 10 
+        }
+    ]
+    SCHEDULER_API_ENABLED = True
 
 @app.route('/setup')
 def webapi():
@@ -23,6 +47,7 @@ def setDataCOM01():
     if request.method == "POST":
         data = {
             'appInfo': {
+                'COM01_Status': request.form['COM01_Status'],
                 'COM01_BaudRate': request.form['COM01_BaudRate'],
                 'COM01_DataSize': request.form['COM01_DataSize'],
                 'COM01_Parity': request.form['COM01_Parity'],
@@ -40,6 +65,7 @@ def setDataCOM02():
     if request.method == "POST":
         data = {
             'appInfo': {
+                'COM02_Status': request.form['COM02_Status'],
                 'COM02_BaudRate': request.form['COM02_BaudRate'],
                 'COM02_DataSize': request.form['COM02_DataSize'],
                 'COM02_Parity': request.form['COM02_Parity'],
@@ -101,5 +127,26 @@ def setDataMqtt01():
         f.close
         return jsonify(result='OK')
 
+def publish_PowerMeter(a, b):
+    
+    FET_MQTT.MqttPublish()
+    
+def read_com1(a, b):
+    '''
+    with open('static/data/COM01.json', 'r') as f:
+        com1_infor = json.load(f)
+    f.close
+    if com1_infor["appInfo"]["COM01_Status"] == "Enable":
+       modbus_data = FET_modbusrtu.getCom1_Power('COM3',int(com1_infor["appInfo"]["COM01_BaudRate"]),104,'INPUT')
+       print (type(modbus_data))
+       print (modbus_data)
+       #print (FET_MQTT.Pub_infor())
+    '''
+    
 if __name__ == '__main__':
+    
+    app.config.from_object(Config())
+    scheduler = APScheduler()
+    scheduler.init_app(app) 
+    scheduler.start()    
     app.run('0.0.0.0', debug=True)
