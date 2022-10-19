ff8ffd459e6a18021fdc899c6fa8f4fabab53641
TacoJimmy <t5669016@ntut.org.tw>
2022-10-19 14:13:49 +0800

    Update FET_MQTT.py

--- FET_MQTT.py
+++ FET_MQTT.py
@@ -1,37 +1,92 @@
-    
+# coding:utf-8
 import codecs
 import json
 import ssl
 import paho.mqtt.client as mqtt
 import time
+import FET_modbustcp
 
-def publish_mqtt():
-    meter_token = 'smartbuilding_device'
-    FETnet_passwd = '?3NXw+Pp'
+def PowerLoop():
+    with open('static/data/PowerMeter.json', 'r') as f:
+        data = json.load(f)
+    f.close
+    return data
 
-    data01  = [
-        {"access_token": "WImETF1BotX8l1xIkZ3K",
-         "app": "ems_demo_fet",
-         "ts": 1665172851000,
-         "type": "METER",
-         "data": [{"values": {"bb": "22","demand": "50","voltage": "380","power": "300","current_r": "20","current_s": "20","current_t": "20"}}]}]
+def ReadMqttInfor():
+    with open('static/data/mqttinfor.json', 'r') as f:
+        data = json.load(f)
+    f.close
+    return data
+
+def MqttSend(mod_payload):
+    Mqttinfor = ReadMqttInfor()
     try:
         client = mqtt.Client('', True, None, mqtt.MQTTv31)
-        client.username_pw_set(meter_token, FETnet_passwd)
+        client.username_pw_set(Mqttinfor['appInfo']['MQTT_UserName'], Mqttinfor['appInfo']['MQTT_Password'])
         context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         client.tls_set_context(context)
-        client.connect("mqttd.fetnet.net", 8883, 60)
+        client.connect(Mqttinfor['appInfo']['MQTT_url'], Mqttinfor['appInfo']['MQTT_Port'], 60)
         client.loop_start()
         time.sleep(1)
-        client.on_connect
-        client.publish('/ems/v1/telemetry/demoNH468',json.dumps(data01))
+        data02 = client.on_connect
+        data03 = client.publish(Mqttinfor['appInfo']['MQTT_topic'],json.dumps(mod_payload[0]))
+        time.sleep(1)
+        data03 = client.publish(Mqttinfor['appInfo']['MQTT_topic'],json.dumps(mod_payload[1]))
+        time.sleep(1)
+        data03 = client.publish(Mqttinfor['appInfo']['MQTT_topic'],json.dumps(mod_payload[2]))
+        time.sleep(1)
+        client.loop_stop()
+        client.disconnect()
+        time.sleep(1)
+    except:
+        print ('error')
+        return ('error')
+def MqttPublish():
+    try:
+        #PowerInfor = PowerLoop()
+        
+        MainLoop01 = FET_modbustcp.getPowerLoop01('192.168.1.10',502)
+        MqttSend(MainLoop01)
+        MainLoop02 = FET_modbustcp.getPowerLoop02('192.168.1.11',502)
+        MqttSend(MainLoop02)
+                
+        print('ok')
+        return ('OK')
+        
+    except:
+        print ('error')
+        return ('error')
+
+def Pub_infor():
+    try:
+        Mqttinfor = ReadMqttInfor()
+        PowerInfor = PowerLoop()
+        MainLoop01  = [
+            {"access_token": PowerInfor["MainLoop01"]["access_token"],
+             "app": PowerInfor["MainLoop01"]["app"],
+             "type": PowerInfor["MainLoop01"]["type"],
+             "data": PowerInfor["MainLoop01"]["data"]}]
+        print (Mqttinfor['appInfo']['MQTT_UserName'])
+    
+        client = mqtt.Client('', True, None, mqtt.MQTTv31)
+        client.username_pw_set(Mqttinfor['appInfo']['MQTT_UserName'], Mqttinfor['appInfo']['MQTT_Password'])
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        client.tls_set_context(context)
+        client.connect(Mqttinfor['appInfo']['MQTT_url'], Mqttinfor['appInfo']['MQTT_Port'], 60)
+        client.loop_start()
+        time.sleep(1)
+        data02 = client.on_connect
+        data03 = client.publish(Mqttinfor['appInfo']['MQTT_topic'],json.dumps(MainLoop01))
         time.sleep(3)
         client.loop_stop()
         client.disconnect()
+        time.sleep(10)
+        return ('OK')
     except:
-        print ("error")
-        #return ('loss_connect')
-
-while True:
-    publish_mqtt()
-    time.sleep(60)
+        return ('error')
+        
+if __name__ == '__main__':
+    while True:
+        #PowerLoop()
+        MqttPublish()
+        time.sleep(10)
