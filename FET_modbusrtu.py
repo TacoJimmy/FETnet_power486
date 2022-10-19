b41fda47ea128a5c86551772b6cacde777894554
TacoJimmy <t5669016@ntut.org.tw>
2022-10-19 18:13:15 +0800

    Update FET_modbusrtu.py

--- FET_modbusrtu.py
+++ FET_modbusrtu.py
@@ -43,6 +43,46 @@ def read_3p3w_meter(PORT,ID,loop):
         master.close()
         time.sleep(0.5)
         return (MainPW_meter)
+    
+    except:
+        MainPW_meter[0] = 0
+        MainPW_meter[1] = 0
+        MainPW_meter[2] = 0
+        MainPW_meter[3] = 0
+        MainPW_meter[4] = 0
+        MainPW_meter[5] = 0
+        MainPW_meter[6] = 0
+        MainPW_meter[7] = 2
+        master.close()
+        time.sleep(0.5)
+        return (MainPW_meter)
+
+def read_Main_PowerMeter(PORT,ID,loop):
+    loop = loop - 1
+    MainPW_meter = [0,0,0,0,0,0,0,0]
+    try:
+        master = modbus_rtu.RtuMaster(serial.Serial(port=PORT, baudrate=9600, bytesize=8, parity='N', stopbits=1, xonxoff=0))
+        master.set_timeout(5.0)
+        master.set_verbose(True)
+        pw_va = master.execute(ID, cst.READ_HOLDING_REGISTERS, 304, 1)
+        #pw_cur = master.execute(ID, cst.READ_HOLDING_REGISTERS, 5+loop*4, 3)
+        #pw_power = master.execute(ID, cst.READ_HOLDING_REGISTERS, 15+loop*10, 1)
+        #pw_pf = master.execute(ID, cst.READ_HOLDING_REGISTERS, 24+loop*12, 1)
+        #pw_consum = master.execute(ID, cst.READ_HOLDING_REGISTERS, 39+loop*4, 2)
+        
+        MainPW_meter[0] =  round(pw_va[0] * 0.1,1)
+
+        #MainPW_meter[0] =  round(pw_va[0] * 0.1,1)
+        #MainPW_meter[1] =  round(pw_cur[0] * 0.01,1)
+        #MainPW_meter[2] =  round(pw_cur[1] * 0.01,1)
+        #MainPW_meter[3] =  round(pw_cur[2] * 0.01,1)
+        #MainPW_meter[4] =  round(pw_power[0] * 0.01,1) 
+        #MainPW_meter[5] =  round(pw_pf[0]*0.001,1)
+        #MainPW_meter[6] =  round((pw_consum[1] + pw_consum[0] * 65535)*0.1,1)
+        #MainPW_meter[7] = 1 
+        master.close()
+        #time.sleep(0.5)
+        return (MainPW_meter)
 
     except:
         MainPW_meter[0] = 0
@@ -55,10 +95,13 @@ def read_3p3w_meter(PORT,ID,loop):
         MainPW_meter[7] = 2
         master.close()
         time.sleep(0.5)
-        return (MainPW_meter) 
+        return (MainPW_meter)
+ 
 if __name__ == '__main__':
     #print (getCom1_Power('/dev/ttyS4',9600,3,'INPUT'))
     #air condition 1
     print (read_3p3w_meter('/dev/ttyS1',3,1))
     #air condition 2
     print (read_3p3w_meter('/dev/ttyS1',4,1))
+    
+    read_Main_PowerMeter('/dev/ttyS1',1,1)
