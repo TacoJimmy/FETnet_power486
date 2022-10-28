7c5ea40684d109055c8c1eaa84c9fd5c959885f3
TacoJimmy <t5669016@ntut.org.tw>
2022-10-28 19:06:06 +0800

    Update FET_Flask.py

--- FET_Flask.py
+++ FET_Flask.py
@@ -268,8 +268,8 @@ if __name__ == '__main__':
     
     live_server = Server(app.wsgi_app)
     live_server.watch('**/*.*')
-    live_server.serve(host='0.0.0.0',open_url_delay=True)
-    live_server.serve(open_url=False, live_css=False, host='0.0.0.0', debug=True)
+    #live_server.serve(host='0.0.0.0',open_url_delay=True)
+    live_server.serve(open_url=False,open_url_delay=None, live_css=False, host='0.0.0.0', debug=True)
         
     #app.run('0.0.0.0', debug=True)
     
