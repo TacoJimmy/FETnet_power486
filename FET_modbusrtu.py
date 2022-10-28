147f1578ae98e8425c93fd0652cc87d2869a97e2
TacoJimmy <t5669016@ntut.org.tw>
2022-10-28 18:42:41 +0800

    Update FET_modbusrtu.py

--- FET_modbusrtu.py
+++ FET_modbusrtu.py
@@ -91,8 +91,9 @@ def read_Main_PowerMeter(PORT,ID,loop):
         MainPW_meter[2] = round(pw_cur[3] * 0.001,1)
         MainPW_meter[3] = round(pw_cur[5] * 0.001,1)
         MainPW_meter[4] = round(pw_power[0] * 0.01,1)
-        MainPW_meter[5] = ReadFloat((0,pw_pf[0]))
-        MainPW_meter[6] = round((pw_consum[1] + pw_consum[0] * 65535)*0.1,1)
+        MainPW_meter[5] = pw_pf[0]
+        MainPW_meter[5] = ReadFloat((pw_consum[1],pw_consum[0]))
+        #MainPW_meter[6] = round((pw_consum[1] + pw_consum[0] * 65535)*0.1,1)
         #MainPW_meter[6] = round(pw_consum[0],1)
         MainPW_meter[7] = 1
         #MainPW_meter[0] =  round(pw_va[0] * 0.1,1)
